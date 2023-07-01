311435eeaa15814297599def489f1b579bf053d6
Tu Huu Vo <tuvh@vihatgroup.com>
2023-07-01 17:04:05 +0700

    add

--- src/main/java/com/example/security/repository/StudentRepository.java
+++ src/main/java/com/example/security/repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 
     List<Student> findAllByName(String name);
--- src/main/java/com/example/security/service/StudentService.java
+++ src/main/java/com/example/security/service/StudentService.java
@@ -3,14 +3,17 @@ package com.example.security.service;
 import com.example.security.models.Student;
 import com.example.security.repository.StudentRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+//@NoArgsConstructor
+@RequiredArgsConstructor
 public class StudentService {
-
 
     private final StudentRepository repo;
 
